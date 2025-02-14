8a15b8dcbbaf07199687bf86e7910fadb33ca4f4
naborowiak <naborowiak@gmail.com>
2025-02-14 02:31:34 -0600

    Cleanup dependencies and remove unused configuration references

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-/// <reference types="@astrojs/image/client" />
 
 interface ImportMetaEnv {
   readonly PUBLIC_FIREBASE_API_KEY: string;
